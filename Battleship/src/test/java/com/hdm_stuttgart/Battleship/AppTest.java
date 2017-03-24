65c9ab78186f97b425fedbe9cfa0d57bcddccbdd
Baumgärtner Lea <lb092@hdm-stuttgart.de>
2017-03-24 09:13:04 +0100

    First Version

---
+++ Battleship/src/test/java/com/hdm_stuttgart/Battleship/AppTest.java
@@ -0,0 +1,17 @@
+package com.hdm_stuttgart.Battleship;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Dummy test method
+     */
+    @Test
+    public void testApp() {
+        Assert.assertTrue( true );
+    }
+}
