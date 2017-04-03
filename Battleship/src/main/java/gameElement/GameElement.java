86d1abec0f217dd357652da5380f6ab933b1db9a
Baumgärtner Lea <lb092@hdm-stuttgart.de>
2017-04-03 16:56:19 +0200

    Version 0.0v - Tiny prprogress in classes

--- Battleship/src/main/java/gameElement/GameElement.java
+++ Battleship/src/main/java/gameElement/GameElement.java
@@ -1,5 +1,20 @@
 package gameElement;
 
 public abstract class GameElement {
+	
+		int size;
+		int posX;
+		int posY;
+		
+		public void setRandomPosition() {
+			
+		}
+		
+		public void getRandomPosition() {
+			
+		}
+		
+		
+		
 
 }
