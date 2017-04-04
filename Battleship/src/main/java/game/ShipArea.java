bd310b9dbd94e4fffa5920e4fdf983cf10e4d4b8
Wichmann Celine <cw089@hdm-stuttgart.de>
2017-04-04 09:47:22 +0200

    Progress in Classes

--- Battleship/src/main/java/game/ShipArea.java
+++ Battleship/src/main/java/game/ShipArea.java
@@ -1,5 +1,15 @@
 package game;
 
+import gameElement.Ship;
+
 public class ShipArea extends GameArea {
 
+	void setShip(Ship ship, int x, int y) {
+		
+	}
+	
+	void removeShip (int x, int y) {
+		
+	}
+	
 }
