88fd286e81aa74c04527dda305916be61d033771
Baumgärtner Lea <lb092@hdm-stuttgart.de>
2017-04-11 08:53:18 +0200

    Version 0.1 - Further Progress in Classes

--- Battleship/src/main/java/gameElement/Dolphine.java
+++ Battleship/src/main/java/gameElement/Dolphine.java
@@ -1,29 +1,19 @@
 package gameElement;
 
-import game.Player;
 
 public class Dolphine extends GameElement{
 //  Impact on the Game: If a Player shots a Dolphine, he / she will get minus points.
-	
-	int size;
-	
 	
 	Dolphine() {
 		super(1);					// Dolphine has ID 1
 		GameElement.getSize();		// get his size
 	}
 	
-	public void dolphinImpact() {
-		int points;
+	public int dolphinImpact() {
 		int minusPoints = -100;	// Punkte sind noch variable festzulegen
-		
+		return minusPoints;
 		
 	}
 	
-	
-	/*public  int dolphineImpact() {
-		int hits;
-		// hits = Player.getCountedPoints(Player.countPoints());
-	}*/
 }
 
--- Battleship/src/main/java/gameElement/Island.java
+++ Battleship/src/main/java/gameElement/Island.java
@@ -2,9 +2,21 @@ package gameElement;
 
 public class Island extends GameElement{
 
-	public Island(int numberGameElement) {
-		super(numberGameElement);
-		numberGameElement = 2;
+	Island() {
+		super(2);					// Island has ID 2
+		GameElement.getSize();		// get his size
+	}
+	
+	public int IslandImpact() {
+		// We want to change the way the player is able to shoot - instead of shooting on one field
+		// Shooting on four fields for 3 Rounds
+		
+		int x = 4;
+		int y;
+		
+								// Punkte sind noch variable festzulegen
+		return x;
+		
 	}
 
 }
