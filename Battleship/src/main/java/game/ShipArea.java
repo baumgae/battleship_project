f1bab5a66a0be78dc27dc4c344902a982b56fbed
Tobias Jordine <jordine@hdm-stuttgart.de>
2017-04-18 15:15:28 +0200

    Restored ShipArea.java

---
+++ Battleship/src/main/java/game/ShipArea.java
@@ -0,0 +1,15 @@
+package game;
+
+import ships.IShip;
+
+public class ShipArea extends GameArea {
+
+	void setShip(IShip ship, int x, int y) {
+		
+	}
+	
+	void removeShip (int x, int y) {
+		
+	}
+	
+}
