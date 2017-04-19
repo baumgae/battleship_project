fa66ad2b3dad56acf3412c6ef3e696833a31241e
Baumgärtner Lea <lb092@hdm-stuttgart.de>
2017-04-19 17:12:06 +0200

    Version 0.2 - add first documentation

--- Battleship/src/main/java/gameElement/AGameItems.java
+++ Battleship/src/main/java/gameElement/AGameItems.java
@@ -1,16 +1,55 @@
 package gameElement;
-
+/**
+ * Abstract Class for Game Items
+ * <p>
+ * This is the super class for the Game Items.
+ * It sets the size of every Element on 1, which means
+ * that every Game Item is one field big.
+ * Due to the constructor the Children have to set an ID.
+ * And there are methods for getting the size and the ID.
+ 
+ * <p>
+ * Subclasses use constructor {@link #AGameItems} to set the ID
+ * Use {@link #getSize} to get the size of a game item
+ * Use {@link #getGameElementID} to get the ID of a game item
+ * 
+ * <p> 
+ * In case of pre-existing entries {@link #importDb} might be preferable
+ *
+ * @author Lea Baumgärtner
+ * @version 0.2 
+ */
 
 public abstract class AGameItems implements IGameItem { 
-	
+		
+	/**
+	*
+	* Fields for the GameItems
+	*
+	**/
 		private int IDGameElement;
 		final static int size = 1;
 		
-		
+	/**
+	*
+	* Super Constructor for the subclasses
+	* 
+	* @param IDGameElement
+	*
+	**/	
 		//Constructor
 		AGameItems (int IDGameElement) {
 			this.IDGameElement = IDGameElement;
 		}
+	
+	/**
+	*
+	* Methods for getting size and GameElement ID
+	* 
+	* @return size of the Game Item
+	* @return ID of the Game Item
+	* 
+	**/
 		
 		/* (non-Javadoc)
 		 * @see gameElement.IGameItem#getSize()
