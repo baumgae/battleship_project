3754a686aef67e5ca4a2cfe15e64230eca59caae
Baumgärtner Lea <lb092@hdm-stuttgart.de>
2017-05-16 17:35:13 +0200

    Progress in Testing

--- Battleship/src/test/java/com/hdm_stuttgart/Battleship/PlayerFactoryTest.java
+++ Battleship/src/test/java/com/hdm_stuttgart/Battleship/PlayerFactoryTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import gameConfigurations.ComputerPlayer;
 import gameConfigurations.CreatePlayerException;
 import gameConfigurations.HumanPlayer;
 import gameConfigurations.IPlayer;
@@ -12,12 +13,43 @@ import junit.framework.Assert;
 
 public class PlayerFactoryTest {
 	
+	
+	
 	@Test
-	public void testWrongPlayerchoosen() throws CreatePlayerException {
+	public void testRightPlayerchoosen() throws CreatePlayerException {
 	
 		IPlayer Player = PlayerFactory.getInstance("human", "Celine");
 		Assert.assertTrue(Player instanceof HumanPlayer);
 	}
+	// Expected right
+	
+	@Test
+	public void testWrongPlayerchoosen() throws CreatePlayerException {
+	
+		IPlayer Player = PlayerFactory.getInstance("ai", "ComputerHans");
+		Assert.assertTrue(Player instanceof HumanPlayer);
+	}
+	// Expected wrong
+	
+	
+	@Test
+	public void testRightComputerPlayerchoosen() throws CreatePlayerException {
+	
+		IPlayer Player = PlayerFactory.getInstance("ai", "ComputerHans");
+		Assert.assertTrue(Player instanceof ComputerPlayer);
+	}
+	// expected right
+	
+	
+	@Test
+	public void testCompletelyWrong() throws CreatePlayerException {
+	
+		IPlayer Player = PlayerFactory.getInstance("xyz", "Luigi");
+		Assert.assertTrue(Player instanceof HumanPlayer);
+	}
+	
+	// expected wrong
+	
 	
 	// noch zwei tests 
 	// einmal für CompiuterPlayer und dann für xy falsches Zeug
