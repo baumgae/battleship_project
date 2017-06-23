3a3cc6c522d530d901c5c99850b7c1c33a8b1152
Baumgärtner Lea <lb092@hdm-stuttgart.de>
2017-06-23 08:53:14 +0200

    Added some comments

--- Battleship/src/main/java/com/hdm_stuttgart/Battleship/CustomButtonTest2.java
+++ Battleship/src/main/java/com/hdm_stuttgart/Battleship/CustomButtonTest2.java
@@ -25,114 +25,104 @@ import javafx.stage.Stage;
  * 
  */
 
-	import org.apache.logging.log4j.LogManager;
-	import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import game.DifficultyManager;
+import game.EDifficulty;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-	import game.DifficultyManager;
-	import game.EDifficulty;
-	import javafx.scene.control.Label;
-	import javafx.scene.layout.HBox;
-	import javafx.scene.layout.VBox;
-	import javafx.stage.Stage;
-
-	public class CustomButtonTest2 extends Application{
+public class CustomButtonTest2 extends Application {
 
 	private static final Logger logger = LogManager.getLogger(CustomButtonTest2.class);
-		
-		private static Stage primaryStage;
-		
-		VBox root;
-		VBox root3;
-		VBox root2;
-		
-		
-//		public class CustomButtonTest2 extends Application{
-//			
-//			
-//			private static Stage primaryStage;
-//			int i;
-		
-		 public static Stage getPrimaryStage() {
-			 return primaryStage;
-		 }
-		 
-		 private GridPane grid = new GridPane();
-		 
-		 @Override
-			public void start(Stage primaryStage) throws Exception {
-				
-				this.primaryStage = primaryStage;
-				
-				
-				
-				/**
-				 * Title for the whole screen
-				 */
-				Label labelTitle = new Label("Battleship");
-				
-				Point difficulty = DifficultyManager.getFieldSize(EDifficulty.EASY);
-				
-				CustomButton buttons [][] = new CustomButton[difficulty.x][difficulty.y];
-				
-				
-				for (int i = 0; i < buttons.length; i++) {
-					for (int j = 0; j < buttons[i].length; j++) {
-					Point p = new Point(i, j);
-					buttons[i][j]= new CustomButton(p,0);
-				
-					buttons[i][j].setOnAction(event ->{
-						CustomButton clickedButton = (CustomButton) event.getSource();
-						clickedButton.unhide();
-					});
-					
-					
-					grid.add(buttons[i][j], i, j);
-					
-		
-					}
-				}
-					
-				Scene scene = new Scene (grid);
-		        primaryStage.setScene(scene);
-		        primaryStage.show();
-	
-	
-//	@Override
-//    public void start(Stage primarystage) throws Exception {
-//        Pane root = new Pane();
-//        i = 0;
-//        while(i<5){
-//            addButton();
-//        }
-//        root.getChildren().add(gpnael);
-//        Scene scene = new Scene(root);
-//        primarystage.setScene(scene);
-//        primarystage.show();
-//    }
-//	
-//	
-//    private void addButton() {
-//        i++;
-//       // Array aus buttons - doesn't work"
-//        Point p = new Point(0,0);
-//        CustomButton custom = new CustomButton(p);
-//        
-//       custom.setOnAction(event ->{
-//			CustomButton clickedButton = (CustomButton) event.getSource();
-//			clickedButton.unhide(3);
-//		});
-//        
-//        // gpnael.add(custom, i, i);
-//        gpnael.getChildren().add(custom);
-//        
-//    }
-		 }
-		 
+
+	private static Stage primaryStage;
+
+	VBox root;
+	VBox root3;
+	VBox root2;
+
+	// public class CustomButtonTest2 extends Application{
+	//
+	//
+	// private static Stage primaryStage;
+	// int i;
+
+	public static Stage getPrimaryStage() {
+		return primaryStage;
+	}
+
+	private GridPane grid = new GridPane();
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		this.primaryStage = primaryStage;
+
+		/**
+		 * Title for the whole screen
+		 */
+		Label labelTitle = new Label("Battleship");
+
+		Point difficulty = DifficultyManager.getFieldSize(EDifficulty.EASY);
+
+		CustomButton buttons[][] = new CustomButton[difficulty.x][difficulty.y];
+
+		for (int i = 0; i < buttons.length; i++) {
+			for (int j = 0; j < buttons[i].length; j++) {
+				Point p = new Point(i, j);
+				buttons[i][j] = new CustomButton(p, 0);
+
+				buttons[i][j].setOnAction(event -> {
+					CustomButton clickedButton = (CustomButton) event.getSource();
+					clickedButton.unhide();
+				});
+
+				grid.add(buttons[i][j], i, j);
+
+			}
+		}
+
+		Scene scene = new Scene(grid);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		// @Override
+		// public void start(Stage primarystage) throws Exception {
+		// Pane root = new Pane();
+		// i = 0;
+		// while(i<5){
+		// addButton();
+		// }
+		// root.getChildren().add(gpnael);
+		// Scene scene = new Scene(root);
+		// primarystage.setScene(scene);
+		// primarystage.show();
+		// }
+		//
+		//
+		// private void addButton() {
+		// i++;
+		// // Array aus buttons - doesn't work"
+		// Point p = new Point(0,0);
+		// CustomButton custom = new CustomButton(p);
+		//
+		// custom.setOnAction(event ->{
+		// CustomButton clickedButton = (CustomButton) event.getSource();
+		// clickedButton.unhide(3);
+		// });
+		//
+		// // gpnael.add(custom, i, i);
+		// gpnael.getChildren().add(custom);
+		//
+		// }
+	}
+
 	public static void main(String[] args) {
-        launch(args);
-    }
-	
+		launch(args);
+	}
+
 }
-
-
--- Battleship/src/main/java/gameConfigurations/PlayerFactory.java
+++ Battleship/src/main/java/gameConfigurations/PlayerFactory.java
@@ -24,14 +24,6 @@ public class PlayerFactory {
 	public static final String PLAYER_TYPE_HUMAN = "human";
 	private static final Logger logger = LogManager.getLogger(PlayerFactory.class);
 
-	/*
-	 * Was ist der Unterschied? Logger logger = LogManager.getRootLogger();
-	 * private static final Logger logger =
-	 * LogManager.getLogger(DifficultyManager.class);
-	 */
-	
-	// RuntimeException - soll ich lieber programmieren?
-	
 	public static IPlayer getInstance(String type, String name) throws CreatePlayerException {
 		if (type == null) {
 			logger.error("No Player has been created and CreatePlayerException was thrown");
@@ -41,10 +33,10 @@ public class PlayerFactory {
 
 			logger.info("A new HumanPlayer has been created!");
 			return new HumanPlayer(name);
-			
+
 		} else if (type.equals(PLAYER_TYPE_AI)) {
 			logger.info("A new ComputerPlayer has been created!");
-			return new ComputerPlayer("ComputerHans"); // Erstmal nicht aktiv
+			return new ComputerPlayer("ComputerHans");
 
 		} else {
 			logger.error("No Player has been created and CreatePlayerException was thrown");
@@ -52,14 +44,5 @@ public class PlayerFactory {
 
 		}
 	}
-
-	/*
-	 * I think about throwing new CreatePlayerExceptions in the if == null and
-	 * the else. Should I do it with a try catch Block?
-	 * 
-	 * 
-	 * try { Player player = new HumanPlayer(); LOG } catch
-	 * (CreatePlayerException e) { LOG;
-	 */
 
 }
