05d6c2202965e81d5827161b52f98fd682b94201
Wichmann Celine <cw089@hdm-stuttgart.de>
2017-06-27 12:37:43 +0200

    Create new Class

---
+++ Battleship/src/main/java/com/hdm_stuttgart/Battleship/StreamNames.java
@@ -0,0 +1,13 @@
+package com.hdm_stuttgart.Battleship;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class StreamNames {
+	
+	//aus dem SetNameScreen, string name holen in der collection, z.b Liste speichern,
+	//filtern und nach namen sortieren
+	Collection<String> storeNames = new ArrayList<String>();
+	
+
+}
