1a8dee3c2b143ff80d0a93b36e3884cbaa96d9db
Wichmann Celine <cw089@hdm-stuttgart.de>
2017-06-28 22:49:12 +0200

    Working in Class Highscore

---
+++ Battleship/src/main/java/com/hdm_stuttgart/Battleship/Highscore.java
@@ -0,0 +1,31 @@
+package com.hdm_stuttgart.Battleship;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import gameConfigurations.HumanPlayer;
+
+public class Highscore {
+	
+	//Instanzen Player vom GameManager und den Namen sammt den Punkten speichern
+	//werte von player 1 und player 2 vergleichen
+	//das höhere wird mit namen in einer liste gespeichert
+	// sortieren
+	public void setHighscore(String name, int points) {
+	List<String> namesList = new ArrayList<String>();
+	namesList.add(name);
+	
+//	Map<Integer, String> highscoreList = namesList
+//	.stream()
+//	.collect(Collectors.toMap(points, name))
+//	.filter();
+	
+	}
+	
+	public void getHighsore(){
+		
+	}
+	
+}
